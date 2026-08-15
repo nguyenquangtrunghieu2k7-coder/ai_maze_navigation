f8f8e3bc86def8faddc6f6fd30f8439d81c4d9d7
TrHieu <nguyenquangtrunghieu2k7@gmail.com>
2026-08-15 21:17:51 +0700

    update gen maze

--- generation/recursive_backtracking.py
+++ generation/recursive_backtracking.py
@@ -1,11 +1,39 @@
 #recursive_backtracking.py là module sinh mê cung
 import random
 from core.maze import Maze
+from collections import deque
+
+def find_farthest_cell(maze, start):
+    queue = deque([start])
+    visited ={start}
+    distance = {start: 0}
+
+    farthest = start
+    while queue:
+        current = queue.popleft()
+        farthest = current 
+
+        x,y = current
+        for nx,ny in maze.neighbors(x,y):
+            if (nx,ny) not in visited:
+                visited.add((nx,ny))
+                distance[(nx,ny)] = distance[current] + 1
+                queue.append((nx,ny))
+
+    return farthest
+
+
 
 class RecursiveBacktrackingGeneraion:
     def __init__(self, seed: int | None=None):
+        #Seed là giá trị khởi tạo của bộ sinh số ngẫu nhiên.
+        #Khi co seed => Lần nào chạy cũng ra cùng kết quả.
+        #Các thí nghiệm được thực hiện trên tập mê cung cố định sinh bởi seed xác định. => same maze for all tests
         self.random = random.Random(seed)
     def generate(self, width, height) -> Maze:
+        #Thuật toán Recursive Backtracking coi. ô lẻ = cell, ô chẵn = wall
+        #=> biên luôn là tường, cell luôn ở vị trí lẻ, bước ±2 luôn hợp lệ
+        #Đây là convention của perfect maze.
         if width%2 == 0:
             width += 1
         if height%2 == 0:
@@ -13,7 +41,7 @@ class RecursiveBacktrackingGeneraion:
         maze = Maze(width,height)
         self._carve(Maze)
         maze.start = (1,1)
-        maze.goal = (width -2, height -2)
+        maze.goal = find_farthest_cell(maze, maze.start)
 
         return maze
     def _carve(self, maze: Maze):
@@ -38,4 +66,3 @@ class RecursiveBacktrackingGeneraion:
                 stack.append((nx,ny))
             else: 
                 stack.pop()
-                
