380dea4282c88c1af5ba0744fe9bf9ac586e3378
TrHieu <nguyenquangtrunghieu2k7@gmail.com>
2026-08-16 07:02:09 +0700

    sua base.py 1 ti

--- algorithms/base.py
+++ algorithms/base.py
@@ -1,10 +1,10 @@
 #Đây là interface chung
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from core.maze import Maze
 from core.path import PathResult
 
 class SearchAlgorithms(ABC):
     @abstractmethod
-    def search(self, maze: Maze, start: tuple[int,int], goal: tuple[int,int]) -> PathResult:
+    def search(self, maze: Maze, start: tuple[int,int], goal: tuple[int,int],) -> PathResult:
         pass
